dda3f21f738fd3b888c2e39f70e0edbf840330be
Mark garcia <msgarcia@us.ibm.com>
2024-02-13 12:47:40 -0500

    TC100

---
+++ TestProject/Logical/Test Cases.scm/config.scm
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1707846422121" createdUser="Mark"
+    id="-7ae29a6c:18da3821339:-7e27" type="service_component_resource"
+    updatedTimestamp="1707846422121" updatedUser="Mark" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
